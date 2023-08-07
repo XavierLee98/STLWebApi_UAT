9fb1d4a72e641d816f7d2dc91c3e79737362626d
Jun Chua <tjc@myfastrack.net>
2023-08-07 16:49:48 +0800

    Add project files.

---
+++ StarLaiPortal.Module/Controllers/SalesOrderControllers.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace StarLaiPortal.Module.Controllers
+{
+    partial class SalesOrderControllers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PreviewSO = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            // 
+            // PreviewSO
+            // 
+            this.PreviewSO.Caption = "Preview";
+            this.PreviewSO.Category = "ObjectsCreation";
+            this.PreviewSO.ConfirmationMessage = null;
+            this.PreviewSO.Id = "PreviewSO";
+            this.PreviewSO.ToolTip = null;
+            this.PreviewSO.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.PreviewSO_Execute);
+            // 
+            // SalesOrderControllers
+            // 
+            this.Actions.Add(this.PreviewSO);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction PreviewSO;
+    }
+}
---
+++ StarLaiPortal.Module/Controllers/SalesOrderControllers.cs
@@ -0,0 +1,139 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Web;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using StarLaiPortal.Module.BusinessObjects;
+using StarLaiPortal.Module.BusinessObjects.Sales_Order;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace StarLaiPortal.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class SalesOrderControllers : ViewController
+    {
+        GeneralControllers genCon;
+        public SalesOrderControllers()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+            this.PreviewSO.Active.SetItemValue("Enabled", false);
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+
+            genCon = Frame.GetController<GeneralControllers>();
+            if (View.Id == "SalesOrder_DetailView")
+            {
+                if (((DetailView)View).ViewEditMode == ViewEditMode.View)
+                {
+                    this.PreviewSO.Active.SetItemValue("Enabled", true);
+                }
+            }
+            else
+            {
+                this.PreviewSO.Active.SetItemValue("Enabled", false);
+            }
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        public void openNewView(IObjectSpace os, object target, ViewEditMode viewmode)
+        {
+            ShowViewParameters svp = new ShowViewParameters();
+            DetailView dv = Application.CreateDetailView(os, target);
+            dv.ViewEditMode = viewmode;
+            dv.IsRoot = true;
+            svp.CreatedView = dv;
+
+            Application.ShowViewStrategy.ShowView(svp, new ShowViewSource(null, null));
+
+        }
+        public void showMsg(string caption, string msg, InformationType msgtype)
+        {
+            MessageOptions options = new MessageOptions();
+            options.Duration = 3000;
+            //options.Message = string.Format("{0} task(s) have been successfully updated!", e.SelectedObjects.Count);
+            options.Message = string.Format("{0}", msg);
+            options.Type = msgtype;
+            options.Web.Position = InformationPosition.Right;
+            options.Win.Caption = caption;
+            options.Win.Type = WinMessageType.Flyout;
+            Application.ShowViewStrategy.ShowMessage(options);
+        }
+
+        private void PreviewSO_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            string strServer;
+            string strDatabase;
+            string strUserID;
+            string strPwd;
+            string filename;
+
+            SqlConnection conn = new SqlConnection(genCon.getConnectionString());
+            SalesOrder so = (SalesOrder)View.CurrentObject;
+            ApplicationUser user = (ApplicationUser)SecuritySystem.CurrentUser;
+
+            try
+            {
+                ReportDocument doc = new ReportDocument();
+                strServer = ConfigurationManager.AppSettings.Get("SQLserver").ToString();
+                doc.Load(HttpContext.Current.Server.MapPath("~\\Reports\\SalesOrder.rpt"));
+                strDatabase = conn.Database;
+                strUserID = ConfigurationManager.AppSettings.Get("SQLID").ToString();
+                strPwd = ConfigurationManager.AppSettings.Get("SQLPass").ToString();
+                doc.DataSourceConnections[0].SetConnection(strServer, strDatabase, strUserID, strPwd);
+                doc.Refresh();
+
+                doc.SetParameterValue("dockey@", so.Oid);
+                doc.SetParameterValue("dbName@", conn.Database);
+
+                filename = ConfigurationManager.AppSettings.Get("ReportPath").ToString() + conn.Database
+                    + "_" + so.Oid + "_" + user.UserName + "_SO_"
+                    + DateTime.Parse(so.DocDate.ToString()).ToString("yyyyMMdd") + ".pdf";
+
+                doc.ExportToDisk(ExportFormatType.PortableDocFormat, filename);
+                doc.Close();
+                doc.Dispose();
+
+                string url = HttpContext.Current.Request.Url.Scheme + "://" + HttpContext.Current.Request.Url.Authority +
+                    ConfigurationManager.AppSettings.Get("PrintPath").ToString() + conn.Database
+                    + "_" + so.Oid + "_" + user.UserName + "_SO_"
+                    + DateTime.Parse(so.DocDate.ToString()).ToString("yyyyMMdd") + ".pdf";
+                var script = "window.open('" + url + "');";
+
+                WebWindow.CurrentRequestWindow.RegisterStartupScript("DownloadFile", script);
+            }
+            catch (Exception ex)
+            {
+                showMsg("Fail", ex.Message, InformationType.Error);
+            }
+        }
+    }
+}
