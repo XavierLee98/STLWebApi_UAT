bb9cf39844cd8add3a1e5037a99084cd788f77e2
Jun Chua <tjc@myfastrack.net>
2023-08-17 22:50:03 +0800

    v1.0.8 v4

--- StarLaiPortal.Module/Controllers/NavigationControllers.cs
+++ StarLaiPortal.Module/Controllers/NavigationControllers.cs
@@ -24,31 +24,34 @@ using System.Text;
 namespace StarLaiPortal.Module.Controllers
 {
     // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
-    public partial class NavigationControllers : ViewController
+    public partial class NavigationControllers : WindowController
     {
+        private ShowNavigationItemController showNavigationItemController;
         // Use CodeRush to create Controllers and Actions with a few keystrokes.
         // https://docs.devexpress.com/CodeRushForRoslyn/403133/
         public NavigationControllers()
         {
             InitializeComponent();
             // Target required Views (via the TargetXXX properties) and create their Actions.
+            TargetWindowType = WindowType.Main;
         }
         protected override void OnActivated()
         {
             base.OnActivated();
             // Perform various tasks depending on the target View.
-            ShowNavigationItemController showNavigationItemController = Frame.GetController<ShowNavigationItemController>();
+            showNavigationItemController = Frame.GetController<ShowNavigationItemController>();
             showNavigationItemController.CustomShowNavigationItem += showNavigationItemController_CustomShowNavigationItem;
         }
-        protected override void OnViewControlsCreated()
-        {
-            base.OnViewControlsCreated();
-            // Access and customize the target View control.
-        }
+        //protected override void OnViewControlsCreated()
+        //{
+        //    base.OnViewControlsCreated();
+        //    // Access and customize the target View control.
+        //}
         protected override void OnDeactivated()
         {
             // Unsubscribe from previously subscribed events and release other references and resources.
             base.OnDeactivated();
+            showNavigationItemController.CustomShowNavigationItem -= showNavigationItemController_CustomShowNavigationItem;
         }
 
         void showNavigationItemController_CustomShowNavigationItem(object sender, CustomShowNavigationItemEventArgs e)
